7a63d9db3510a533a53acc043a4c82c4f71eac09
kurehajime <xiidec@gmail.com>
2017-09-24 19:33:42 +0900

    花火追加

---
+++ Assets/GameMaster.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour {
+	GameObject piyoYellow;
+	GameObject goal;
+	GameObject hanabi;
+	Vector3 start = new Vector3 (-8, 1, 0);
+
+	// Use this for initialization
+	void Start () {
+		this.piyoYellow = GameObject.Find ("piyoYellow");
+		this.goal = GameObject.Find ("goal");
+		this.hanabi = GameObject.Find ("hanabi");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (piyoYellow.transform.position.y < -20) {
+			piyoYellow.transform.position = start;
+			piyoYellow.GetComponent<Rigidbody2D> ().velocity = Vector3.zero;
+		}
+		if (isGoal ()) {
+			if (Random.Range ((int)0, (int)2) < 0.5) {
+				piyoYellow.GetComponent<Rigidbody2D> ().velocity = new Vector3 (-80, -80, 0);
+			} else {
+				piyoYellow.GetComponent<Rigidbody2D> ().velocity = new Vector3 (-80, -80, 0);
+			}
+			this.hanabi.GetComponent<HanabiController> ().Fire ();
+		}
+	}
+
+	bool isGoal(){
+		var mag = (piyoYellow.transform.position - goal.transform.position).magnitude;
+
+		if (mag <= 1) {
+			var velo=piyoYellow.GetComponent<Rigidbody2D> ().velocity.magnitude;
+			if(velo<2.0f){
+				return true;
+			}
+		}
+		return false;
+	}
+}
---
+++ Assets/HanabiController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HanabiController : MonoBehaviour {
+	public float fadeTime = 1f;
+	private float currentRemainTime;
+
+	SpriteRenderer spr;
+	// Use this for initialization
+	void Start () {
+		currentRemainTime = 0;
+		this.spr = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentRemainTime -= Time.deltaTime;
+		if ( currentRemainTime <= 0f ) {
+			return;
+		}
+		float alpha = currentRemainTime / fadeTime;
+		var color = spr.color;
+		color.a = alpha;
+		spr.color = color;
+	}
+	public void Fire(){
+		this.currentRemainTime = 1f;
+	}
+}
