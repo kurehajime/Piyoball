92ce5826f1e7fd01eb292cf14a86177aa6d01678
kurehajime <xiidec@gmail.com>
2017-09-24 16:10:12 +0900

    台をちょっと作った

---
+++ Assets/closureController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class closureController : MonoBehaviour {
+	Rigidbody2D rigi2d;
+
+	// Use this for initialization
+	void Start () {
+		rigi2d = GetComponent<Rigidbody2D> ();
+		rigi2d.angularVelocity = 15;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
